921000050f2d6c4ff2bad0c56d5e41c561a8cc67
Arya Kashikar <142014069+aryaaa324@users.noreply.github.com>
2025-06-08 20:14:40 +0530

    Time: 35 ms (39.64%), Space: 14 MB (78.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,8 +4,8 @@ public:
         int n = nums.size();
 
         
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
                 if (nums[i] + nums[j] == target) {
                     return {i, j};
                 }
